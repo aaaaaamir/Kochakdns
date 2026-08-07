48c0c68776f46edc132accec2b3bff7ac8e4b57b
aaaaaamir <amir26076@gmail.com>
2026-08-07 07:23:25 +0000

    eywhwh

---
+++ app/src/main/java/com/example/kochakdns/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kochakdns
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kochakdns.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.text = getString(R.string.hello_text)
+    }
+}
